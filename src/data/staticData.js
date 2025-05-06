10bf011c316c65d4440f60ef0d9529742183ed72
David Bryan <david@opace.co.uk>
2025-05-06 16:16:10 +0100

    fix: export totalCharacters from staticData module

--- src/data/staticData.js
+++ src/data/staticData.js
@@ -1,6 +1,7 @@
 import initialCharacters from './initial-characters.json';
-import { PAGE_SIZE, totalCharacters } from './staticCharacters.js';
+import { PAGE_SIZE, totalCharacters as _totalCharacters } from './staticCharacters.js';
 
+export const totalCharacters = _totalCharacters;
 export const totalPages = Math.ceil(totalCharacters / PAGE_SIZE);
 export const staticCharacters = initialCharacters.slice(0, PAGE_SIZE);
 
